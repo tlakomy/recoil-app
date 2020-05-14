06d4da99e5ccb044076023ff22ce29fd513f756a
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-05-14 21:14:34 +0200

    Finished lesson 01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  useRecoilState,
+} from "recoil";
 import "./App.css";
+
+const numState = atom({
+  key: "numState",
+  default: 0,
+});
+
+function Counter() {
+  const [number, setNumber] = useRecoilState(
+    numState
+  );
+
+  return (
+    <button
+      onClick={() => setNumber(number + 1)}
+    >
+      {number}
+    </button>
+  );
+}
+
+function Display() {
+  const [number] = useRecoilState(numState);
+  return <div>{number}</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recoil!</h1>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <h1>Recoil!</h1>
+        <Counter />
+        <Display />
+      </div>
+    </RecoilRoot>
   );
 }
 
